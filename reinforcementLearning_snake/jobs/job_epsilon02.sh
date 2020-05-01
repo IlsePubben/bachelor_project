ae67bc5c565409362014ecde63dd3225e2bec5db
Ilse <ilse.pubben@gmail.com>
2020-05-01 11:02:07 +0200

    job epsilon02

---
+++ reinforcementLearning_snake/jobs/job_epsilon02.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=20:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=epsilon02
+#SBATCH --output=epsilon02
+#SBATCH --mem=800
+module load Python/3.6.4-foss-2019a
+module load GCCcore/8.2.0
+pip install --user matplotlib
+pip install --user keras
+pip install --user --upgrade tensorflow
+for i in {1..5}
+do 
+    python main.py -a q-learning -e 0.2 -y 0.99
+    python main.py -a qv-learning -e 0.2 -y 0.95
+    python main.py -a qva-learning -e 0.2 -y 0.99
+    python main.py -a qvmax-learning -e 0.2 -y 0.95
+    python main.py -a qvamax-learning -e 0.2 -y 0.95
+done

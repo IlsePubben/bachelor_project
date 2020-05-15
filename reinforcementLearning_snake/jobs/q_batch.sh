063121f6295135e26b55dbf30cdc44e91bd8d352
Ilse_peregrine <ilse.pubben@gmail.com>
2020-05-15 13:09:44 +0200

    finished jobs

--- reinforcementLearning_snake/jobs/q_batch.sh
+++ reinforcementLearning_snake/jobs/q_batch.sh
@@ -14,4 +14,4 @@ pip install --user matplotlib
 pip install --user keras
 pip install --user --upgrade tensorflow
 
-python main.py -a q-learning -e 0.1 -y 0.99 -v 5
+python main.py -a q-learning -e 0.1 -y 0.99 -v 3

15e844225748b79c1e001eff9ca3a54f7a3f0963
Ilse_peregrine <ilse.pubben@gmail.com>
2020-05-27 11:00:02 +0200

    saved models

--- reinforcementLearning_snake/parameters.py
+++ reinforcementLearning_snake/parameters.py
@@ -7,7 +7,7 @@ temperature = 0.1
 initial_snake_length = 3
 game_size = 12
 vision_size = 5
-max_epochs = 200
+max_epochs = 20000
 epoch = 0
 
 start_epsilon = 0.1
@@ -18,8 +18,8 @@ lr_start = 0.005
 lr_end = 0.0005 
 lr_annealing_factor = (lr_end/lr_start)**(1/float(max_epochs))
 learning_rate_q = lr_start
-learning_rate_v = learning_rate_q / 3
-learning_rate_a = learning_rate_q * 3
+learning_rate_v = learning_rate_q
+learning_rate_a = learning_rate_q
 
 model_filepath = ""
 algorithm = "" 
